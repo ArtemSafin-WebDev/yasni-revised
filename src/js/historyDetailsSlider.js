6249316df0246917a866ffc840d25e7a8ec99b18
Artem Safin <artemsafin.webdev@gmail.com>
2019-10-29 01:17:47 +0300

    Закончен блок История успеха

--- src/js/historyDetailsSlider.js
+++ src/js/historyDetailsSlider.js
@@ -1,44 +1,5 @@
 import Swiper from 'swiper';
 
 export default function() {
-    const historyDetailsSlider = Array.from(document.querySelectorAll('.js-history-details-slider'));
-    historyDetailsSlider.forEach(slider => {
-        let swiperInstance = null;
-        const container = slider.querySelector('.swiper-container');
-        const options = {
-            effect: 'fade',
-            fadeEffect: {
-                crossFade: true
-            },
-            watchOverflow: true,
-            navigation: {
-                prevEl: slider.querySelector('.success__history-details-slider-arrow-btn--prev'),
-                nextEl: slider.querySelector('.success__history-details-slider-arrow-btn--next')
-            },
-            pagination: {
-                el: slider.querySelector('.js-history-details-pagination'),
-                type: 'fraction',
-                formatFractionCurrent: number => (number.toString().length === 1 && number !== 0 ? '0' + number : number),
-                formatFractionTotal: number => (number.toString().length === 1 && number !== 0 ? '0' + number : number)
-            }
-        };
-
-        const widthChange = mq => {
-            if (swiperInstance) {
-                swiperInstance.destroy();
-                swiperInstance = null;
-                options.autoHeight = window.matchMedia('(max-width: 768px)') ? true : false;
-                swiperInstance = new Swiper(container, options);
-            } else {
-                options.autoHeight = window.matchMedia('(max-width: 768px)') ? true : false;
-                swiperInstance = new Swiper(container, options);
-            }
-        };
-
-        if (matchMedia) {
-            const mq = window.matchMedia('(max-width: 768px)');
-            mq.addListener(widthChange);
-            widthChange(mq);
-        }
-    });
+    
 }
--- src/js/successSlider.js
+++ src/js/successSlider.js
@@ -1,43 +1,84 @@
 import Swiper from 'swiper';
 
 export default function() {
-    let successSliders = Array.from(document.querySelectorAll('.js-success-slider'));
+    let successSlider = document.querySelector('.js-success-slider');
 
-    successSliders.forEach(block => {
-        const main = block.querySelector('.js-success-slider-main');
-        const thumbs = block.querySelector('.js-success-slider-thumbs');
+    const main = successSlider.querySelector('.js-success-slider-main');
+    const thumbs = successSlider.querySelector('.js-success-slider-thumbs');
 
-        const mainContainer = main.querySelector('.swiper-container');
-        const thumbsContainer = thumbs.querySelector('.swiper-container');
+    const mainContainer = main.querySelector('.swiper-container');
+    const thumbsContainer = thumbs.querySelector('.swiper-container');
 
-        const mainSliderOptions = {
-            navigation: {
-                prevEl: main.querySelector('.success__links-slider-main-arrows-btn--prev'),
-                nextEl: main.querySelector('.success__links-slider-main-arrows-btn--next')
-            },
-            thumbs: {}
-        };
+    const mainSliderOptions = {
+        navigation: {
+            prevEl: main.querySelector('.success__links-slider-main-arrows-btn--prev'),
+            nextEl: main.querySelector('.success__links-slider-main-arrows-btn--next')
+        },
+        thumbs: {}
+    };
 
-        const thumbsSliderOptions = {
-            slidesPerView: 5,
-            // centeredSlides: true,
-            spaceBetween: 20,
-            threshold: 10,
-            watchSlidesVisibility: true,
-            watchSlidesProgress: true,
-            slideToClickedSlide: true,
-            centerInsufficientSlides: true,
-            breakpoints: {
-                
-                769: {
-                    slidesPerView: 7,
-                    spaceBetween: 40,
-                }
+    const thumbsSliderOptions = {
+        slidesPerView: 5,
+        spaceBetween: 20,
+        threshold: 10,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        slideToClickedSlide: true,
+        centerInsufficientSlides: true,
+        breakpoints: {
+            769: {
+                slidesPerView: 7,
+                spaceBetween: 40
             }
-        };
+        }
+    };
 
-        mainSliderOptions.thumbs.swiper = new Swiper(thumbsContainer, thumbsSliderOptions);
+    mainSliderOptions.thumbs.swiper = new Swiper(thumbsContainer, thumbsSliderOptions);
 
-        new Swiper(mainContainer, mainSliderOptions);
-    });
+    const successSliderInstance = new Swiper(mainContainer, mainSliderOptions);
+
+    const detailsSlider = document.querySelector('.js-history-details-slider');
+
+    let detailsSwiperInstance = null;
+    const detailsContainer = detailsSlider.querySelector('.swiper-container');
+    const detailsOptions = {
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        watchOverflow: true,
+        navigation: {
+            prevEl: detailsSlider.querySelector('.success__history-details-slider-arrow-btn--prev'),
+            nextEl: detailsSlider.querySelector('.success__history-details-slider-arrow-btn--next')
+        },
+        pagination: {
+            el: detailsSlider.querySelector('.js-history-details-pagination'),
+            type: 'fraction',
+            formatFractionCurrent: number => (number.toString().length === 1 && number !== 0 ? '0' + number : number),
+            formatFractionTotal: number => (number.toString().length === 1 && number !== 0 ? '0' + number : number)
+        }
+    };
+
+    const widthChange = mq => {
+        if (detailsSwiperInstance) {
+            detailsSwiperInstance.destroy();
+            detailsSwiperInstance = null;
+            detailsOptions.autoHeight = window.matchMedia('(max-width: 768px)') ? true : false;
+            detailsSwiperInstance = new Swiper(detailsContainer, detailsOptions);
+
+            successSliderInstance.controller.control = detailsSwiperInstance;
+            detailsSwiperInstance.controller.control = successSliderInstance;
+        } else {
+            detailsOptions.autoHeight = window.matchMedia('(max-width: 768px)') ? true : false;
+            detailsSwiperInstance = new Swiper(detailsContainer, detailsOptions);
+            successSliderInstance.controller.control = detailsSwiperInstance;
+            detailsSwiperInstance.controller.control = successSliderInstance;
+        }
+    };
+
+    if (matchMedia) {
+        const mq = window.matchMedia('(max-width: 768px)');
+        mq.addListener(widthChange);
+        widthChange(mq);
+    }
 }
